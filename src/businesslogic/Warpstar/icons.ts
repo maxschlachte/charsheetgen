e2a224462817cafbc8edc7adc1adc26d39ade29c
maxschlachte <Maximilian.Schlachte.gz7@fh-zwickau.de>
2025-05-14 10:33:53 +0200

    Add gh pages branch

--- src/businesslogic/Warpstar/icons.ts
+++
@@ -1,6 +0,0 @@
-export enum MDI {
-    RANGED = "mdi-target",
-    MELEE = "mdi-sword",
-    DEFEND = "mdi-shield-outline",
-    DAMAGE = "mdi-water-outline"
-}
--- src/businesslogic/businessLogic.ts
+++
@@ -1,5 +0,0 @@
-import { sheetDef } from "./DSA5/sheetDef";
-//import { sheetDef } from "./Warlock/sheetDef";
-//import { sheetDef } from "./Warpstar/sheetDef";
-
-export const businessLogicSheetDef = sheetDef;
